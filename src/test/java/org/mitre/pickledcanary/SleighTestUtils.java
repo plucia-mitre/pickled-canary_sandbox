a2bd93f7caf779eb918f922c3e629923ea071c6b
wmrobinson <wmrobinson@mitre.org>
2024-10-01 15:33:14 -0400

    Improved method for getting SleighLanguage instance

--- src/test/java/org/mitre/pickledcanary/SleighTestUtils.java
+++ src/test/java/org/mitre/pickledcanary/SleighTestUtils.java
@@ -2,18 +2,12 @@ package org.mitre.pickledcanary;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
 
-import org.antlr.runtime.RecognitionException;
 import org.xml.sax.SAXException;
 
 import generic.jar.ResourceFile;
-import ghidra.app.plugin.processors.sleigh.SleighCompilerSpecDescription;
 import ghidra.app.plugin.processors.sleigh.SleighLanguage;
-import ghidra.pcodeCPort.slgh_compile.SleighCompileLauncher;
-import ghidra.program.model.lang.*;
-import ghidra.program.model.pcode.DecoderException;
-
+import ghidra.app.plugin.processors.sleigh.SleighLanguageProvider;
 
 public class SleighTestUtils {
 	
@@ -28,61 +22,15 @@ public class SleighTestUtils {
 		return new ResourceFile(url.getPath());
 	}
 	
-	/* Compile the .slaspec and return .sla resource
-	 * Returns null if .slaspec can't be located */
-	public static ResourceFile compileSlaSpec(ResourceFile slaSpecFile) 
-			throws DecoderException, UnknownInstructionException, SAXException, IOException {
+	/* Quickly create a SleighLanguage instance from .ldefs */
+	public static SleighLanguage lazyLanguage(ResourceFile lDefsFile) 
+			throws SAXException, IOException {
 		
-		if (slaSpecFile == null) {
-			return null;
-		}
+		/* Constructor is normally only visible in its package, had to edit Ghidra source */
+		SleighLanguageProvider provider = new SleighLanguageProvider(lDefsFile);
 		
-		try {
-			SleighCompileLauncher.runMain(new String[] { slaSpecFile.getAbsolutePath() });
-		}
-		catch (IOException | RecognitionException e) {
-			throw new AssertionError(e);
-			
-		}
-		String slaSpecFileName = slaSpecFile.getName();
-		String slaFileName = slaSpecFileName.substring(0, slaSpecFileName.lastIndexOf('.')) + ".sla";
-		ResourceFile slaFile = getSleighResource(slaFileName);
-		
-		return slaFile;
-	}
-	
-	/* Quickly create a SleighLanguage instance from minimal arguments */
-	public static SleighLanguage lazyLanguage(String langName, Endian endian, int size) 
-			throws DecoderException, UnknownInstructionException, SAXException, IOException {
-
-		/* TODO: check if these actually exist */
-		ResourceFile cSpecFile = getSleighResource(langName + ".cspec");
-		ResourceFile lDefsFile = getSleighResource(langName + ".ldefs");
-		ResourceFile pSpecFile = getSleighResource(langName + ".pspec");
-		ResourceFile slaSpecFile = getSleighResource(langName + ".slaspec");
-		
-		CompilerSpecDescription cSpecDesc =
-			new SleighCompilerSpecDescription(new CompilerSpecID("default"), "default", cSpecFile);
-		
-		ResourceFile slaFile = compileSlaSpec(slaSpecFile);
-		
-		SleighLanguageDescription langDesc = new SleighLanguageDescription(
-			new LanguageID(langName + ":"+ endian.toShortString() +":" + size + ":default"), langName,
-			Processor.findOrPossiblyCreateProcessor(langName), endian, // endian
-			endian, // instructionEndian
-			size, "default", // variant
-			0, // major version
-			0, // minor version
-			false, // deprecated
-			new HashMap<>(), // truncatedSpaceMap
-			new ArrayList<>(List.of(cSpecDesc)), new HashMap<>() // externalNames
-		);
-		
-		langDesc.setDefsFile(lDefsFile);
-		langDesc.setSpecFile(pSpecFile);
-		langDesc.setSlaFile(slaFile);
-		
-		/* constructor is normally package scope, had to edit Ghidra source */
-		return new SleighLanguage(langDesc);
+		/* Get the SleighLanguage we just added to SleighLanguageProvider 
+		 * using LanguageID from its LanguageDescription */
+		return (SleighLanguage) provider.getLanguage(provider.getLanguageDescriptions()[0].getLanguageID());
 	}
 }
