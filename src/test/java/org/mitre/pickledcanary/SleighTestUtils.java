7d6b682c13ab5b81369cf6805d257c9e472d5236
Ben <bpond@mitre.org>
2024-10-03 11:58:04 -0400

    Updating the lazyLanguage method to be able to call a private Ghidra constructor

--- src/test/java/org/mitre/pickledcanary/SleighTestUtils.java
+++ src/test/java/org/mitre/pickledcanary/SleighTestUtils.java
@@ -9,8 +9,10 @@ import generic.jar.ResourceFile;
 import ghidra.app.plugin.processors.sleigh.SleighLanguage;
 import ghidra.app.plugin.processors.sleigh.SleighLanguageProvider;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class SleighTestUtils {
-	
 	/* Retrieves a Sleigh resource from a filename */
 	public static ResourceFile getSleighResource(String name) {
 		URL url = SleighTestUtils.class.getClassLoader().getResource("sleigh/" + name);
@@ -24,11 +26,13 @@ public class SleighTestUtils {
 	
 	/* Quickly create a SleighLanguage instance from .ldefs */
 	public static SleighLanguage lazyLanguage(ResourceFile lDefsFile) 
-			throws SAXException, IOException {
-		
-		/* Constructor is normally only visible in its package, had to edit Ghidra source */
-		SleighLanguageProvider provider = new SleighLanguageProvider(lDefsFile);
-		
+			throws SAXException, IOException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+
+		/* This constructor is private so we need to make it accessible before calling it */
+		Constructor<SleighLanguageProvider> constructor = SleighLanguageProvider.class.getDeclaredConstructor(ResourceFile.class);
+		constructor.setAccessible(true);
+	    SleighLanguageProvider provider = constructor.newInstance(lDefsFile);
+	    
 		/* Get the SleighLanguage we just added to SleighLanguageProvider 
 		 * using LanguageID from its LanguageDescription */
 		return (SleighLanguage) provider.getLanguage(provider.getLanguageDescriptions()[0].getLanguageID());
